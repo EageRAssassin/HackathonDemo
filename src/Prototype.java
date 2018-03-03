45259634b78cf37eec9e03e246f1b5ede509800e
EageRAssassin <rt389@cornell.edu>
2018-03-02 20:06:16 -0500

    Hackathon setup

---
+++ src/Prototype.java
@@ -0,0 +1,42 @@
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+
+public class Prototype {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Prototype window = new Prototype();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public Prototype() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setTitle("Prototype");
+	}
+
+}
